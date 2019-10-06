260e48de43ddef72b0c4354cac95962e1a3b6abe
aquiles <jjoseaquiless@gmail.com>
2019-10-06 14:27:30 -0300

    ac matrix.c

--- matrix.c
+++ matrix.c
@@ -3,9 +3,8 @@
 #include<sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include <sys/stat.h>
+//#include <sys/stat.h>
 #include<stdlib.h>
-
 int A[2000][2000],B[2000][2000],C[2000][2000];
 int filho11 = 0,filho22 = 0,pai = 0,n;
 void product(int size_l, int size_m, int size_n,int ini){
@@ -33,10 +32,12 @@ void product(int size_l, int size_m, int size_n,int ini){
 
 
 void sigfi2(int sig){
-    int i,j;
+    int i = 0,j = 0;
     for(i=n - n/2;i<n;i++){
-        for(j = 0;j<n;j++){
-            printf("%d ",C[i][j]);
+        j = 0;
+        printf("%d",C[i][j]);
+        for(j = 1;j<n;j++){
+            printf(" %d",C[i][j]);
             //fflush(stdout);
         }
         printf("\n");
@@ -44,9 +45,35 @@ void sigfi2(int sig){
     exit(0);
 }
 
+void sigfi1(int sig){
+    int i,j=0;
+    for(i=0;i<n/2;i++){
+        j = 0;
+        printf("%d",C[i][j]);
+        for(j = 1;j<n;j++){
+            printf(" %d",C[i][j]);
+            //fflush(stdout);
+        }
+        printf("\n");
+    }
+    exit(0);
+}
+
+void cont1(int sig){
+    filho11++;
+}
+
+void cont2(int sig){
+    filho22++;
+}
 
 int main(){
-    int i,j,k,sig,status;
+    //freopen("matrix.txt","r",stdin);
+    int i=0,j=0,k,sig,status;
+    sigset_t fi1;
+    sigemptyset(&fi1);
+    sigaddset(&fi1, SIGUSR1); 
+
     scanf("%d",&n);
     for(i=0;i<n;i++){
         for(j = 0;j<n;j++){
@@ -58,32 +85,25 @@ int main(){
             scanf("%d",&B[i][j]);
         }
     }
-    //signal(SIGUSR1,sigfi1);
-    signal(SIGUSR2,sigfi2);
+    signal(SIGUSR1,cont1);
+    signal(SIGUSR2,cont2);
     pid_t filho1 = fork(),filho2;
     if(filho1>0) filho2 = fork();
     else if(filho1 == 0){
         product(n/2,n,n,0);
-        //signal(SIGUSR1,sigmaster);
-        //kill(getppid(),SIGUSR1);
+        signal(SIGUSR1,sigfi1);
+        kill(getppid(),SIGUSR1);
         //sigwait(&usr1,&sig);
-        //pause();
+        pause();
         //printf("filho depois do usr1\n");
         //while(pai != SIGUSR1 );
-
-        for(i=0;i<n/2;i++){
-            for(j = 0;j<n;j++){
-                printf("%d ",C[i][j]);
-                //fflush(stdout);
-            }
-            printf("\n");
-        }
         exit(0);
+
     }
     if(filho2 == 0){
         product(n,n,n,n - n/2);
-        //signal(SIGUSR1,cont);
-        //kill(getppid(),SIGUSR2);
+        signal(SIGUSR1,sigfi2);
+        kill(getppid(),SIGUSR2);
         //sigwait(&usr2,&sig);
         //printf("filho depois do usr2\n");
         pause();
@@ -97,14 +117,15 @@ int main(){
         
         //while(filho11 !=1 );
         //pause();
-        //sigwait(&usr1,&sig);
+        sigwait(&fi1,&sig);
         //printf("pai depois do usr1\n");
-        //kill(filho1,SIGUSR1);
-        //while(filho22 !=1 );
+        kill(filho1,SIGUSR1);
+        wait(0);
+        while(filho22 !=1 );
         //pause();
-        waitpid(filho1,0,0);
+        //waitpid(filho1,NULL,0);
         //printf("pai depois do usr2\n");
-        kill(filho2,SIGUSR2);
+        kill(filho2,SIGUSR1);
         
         wait(0);
 
